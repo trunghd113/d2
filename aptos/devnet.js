e3dc087eba9448a8c8cde2370241251a477906cc
Shinosuke <112403463+trunghd113@users.noreply.github.com>
2025-02-14 14:53:27 +0700

    Add files via upload

---
+++ aptos/devnet.js
@@ -0,0 +1,86 @@
+[
+  {
+    address: '0x1::aptos_coin::AptosCoin',
+    decimals: 8,
+    symbol: 'APT',
+    name: 'Aptos',
+    logoURL: 'https://coinlist.animeswap.org/icons/APT.svg',
+    projectURL: 'https://aptoslabs.com/',
+  },
+  {
+    address: '0x16fe2df00ea7dde4a63409201f7f4e536bde7bb7335526a35d05111e68aa322c::AnimeCoin::ANI',
+    decimals: 8,
+    symbol: 'ANI',
+    name: 'AnimeSwap Coin',
+    logoURL: 'https://coinlist.animeswap.org/icons/ANI.png',
+    projectURL: 'http://animeswap.org/',
+  },
+  {
+    address: '0x16fe2df00ea7dde4a63409201f7f4e536bde7bb7335526a35d05111e68aa322c::TestCoinsV1::USDT',
+    decimals: 8,
+    symbol: 'USDT',
+    name: 'Tether USD',
+    logoURL: 'https://coinlist.animeswap.org/icons/USDT.webp',
+    extensions: {
+      stableUSD: true,
+    },
+  },
+  {
+    address: '0x16fe2df00ea7dde4a63409201f7f4e536bde7bb7335526a35d05111e68aa322c::TestCoinsV1::BTC',
+    decimals: 8,
+    symbol: 'BTC',
+    name: 'Bitcoin',
+    logoURL: 'https://coinlist.animeswap.org/icons/BTC.webp',
+  },
+  {
+    address: '0xfa8c09437aa00eef18849eb9bfabf2be0dac73f03da91f46218cd1647278931b::staked_aptos_coin::StakedAptosCoin',
+    decimals: 8,
+    symbol: 'tAPT',
+    name: 'Tortuga Staked APT',
+    logoURL: 'https://coinlist.animeswap.org/icons/tAPT.svg',
+  },
+  {
+    address: '0xbc954a7df993344c9fec9aaccdf96673a897025119fc38a8e0f637598496b47a::test_mint_dai_coin::TestMintCoin',
+    decimals: 8,
+    symbol: 'ceDAI',
+    name: 'Celer Dai Stablecoin',
+    logoURL: 'https://coinlist.animeswap.org/icons/DAI.webp',
+    extensions: {
+      stableUSD: true,
+    },
+  },
+  {
+    address: '0xbc954a7df993344c9fec9aaccdf96673a897025119fc38a8e0f637598496b47a::test_mint_usdc_coin::TestMintCoin',
+    decimals: 8,
+    symbol: 'ceUSDC',
+    name: 'Celer USD Coin',
+    logoURL: 'https://coinlist.animeswap.org/icons/USDC.webp',
+    extensions: {
+      stableUSD: true,
+    },
+  },
+  {
+    address: '0xbc954a7df993344c9fec9aaccdf96673a897025119fc38a8e0f637598496b47a::test_mint_usdt_coin::TestMintCoin',
+    decimals: 8,
+    symbol: 'ceUSDT',
+    name: 'Celer Tether USD',
+    logoURL: 'https://coinlist.animeswap.org/icons/USDT.webp',
+    extensions: {
+      stableUSD: true,
+    },
+  },
+  {
+    address: '0xbc954a7df993344c9fec9aaccdf96673a897025119fc38a8e0f637598496b47a::test_mint_wbtc_coin::TestMintCoin',
+    decimals: 8,
+    symbol: 'ceWBTC',
+    name: 'Celer Wrapped Bitcoin',
+    logoURL: 'https://coinlist.animeswap.org/icons/BTC.webp',
+  },
+  {
+    address: '0xbc954a7df993344c9fec9aaccdf96673a897025119fc38a8e0f637598496b47a::test_mint_weth_coin::TestMintCoin',
+    decimals: 8,
+    symbol: 'ceWETH',
+    name: 'Celer Wrapped Ethereum',
+    logoURL: 'https://coinlist.animeswap.org/icons/WETH.png',
+  },
+]
